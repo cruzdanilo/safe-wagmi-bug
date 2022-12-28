7dc2d46a71a9ab3201a5dfc8f28489f0cc7a8762
danilo neves cruz <cruzdanilo@gmail.com>
2022-12-27 21:25:59 -0500

    ➕ add `@gnosis.pm/safe-apps-wagmi`

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,7 @@
 import { modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
 import { configureChains, createClient } from 'wagmi'
 import { goerli, mainnet } from 'wagmi/chains'
+import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
 
 export const walletConnectProjectId = '11ddaa8aaede72cb5d6b0dae2fed7baa'
 
@@ -11,7 +12,10 @@ const { chains, provider, webSocketProvider } = configureChains(
 
 export const client = createClient({
   autoConnect: true,
-  connectors: modalConnectors({ appName: 'My wagmi + Web3Modal App', chains }),
+  connectors: [
+    new SafeConnector({ chains }),
+    ...modalConnectors({ appName: 'My wagmi + Web3Modal App', chains })
+  ],
   provider,
   webSocketProvider,
 })
